319251ffe48bcc076803f07a20a600373b0355a3
mktu <mushi9ui@gmail.com>
2022-05-11 22:09:27 +0900

    change puppeteer version

--- libs/chronium.ts
+++ libs/chronium.ts
@@ -18,7 +18,6 @@ async function getPage(isDev: boolean) {
     
     const options = await getOptions(isDev);
     const browser = await puppeteer.launch(options);
-    console.log('launch succeeded')
     _page = await browser.newPage() as Page;
     return _page;
 }
@@ -27,8 +26,6 @@ export async function getScreenshot(html: string, isDev: boolean) {
     const page = await getPage(isDev);
     await page.setViewport({ width: 1200, height: 630 });
     await page.setContent(html, {waitUntil : 'networkidle0'});
-    console.log('setContent')
     const file = await page.screenshot({ type : 'png' });
-    console.log('page.screenshot')
     return file;
 }
