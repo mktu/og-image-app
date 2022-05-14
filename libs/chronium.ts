37c7abf04eacf9d9d527926dbb1e99d9a84866d2
mktu <mushi9ui@gmail.com>
2022-05-14 15:17:59 +0900

    change to playwright

--- libs/chronium.ts
+++ libs/chronium.ts
@@ -1,27 +1,41 @@
-import { getOptions } from './options';
-import { Page } from 'puppeteer-core'
+import * as playwright from "playwright-aws-lambda";
 
-let _page : Page;
+const exePath = process.platform === 'win32'
+? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+: process.platform === 'linux'
+? '/usr/bin/google-chrome'
+: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+
+interface Options {
+    args: string[];
+    executablePath: string;
+    headless: boolean;
+}
+
+export async function getOptions(isDev: boolean) {
+    let options : Options | undefined= undefined;
+    if (isDev) {
+        options = {
+            args: [],
+            executablePath: exePath,
+            headless: true
+        };
+    } 
+    return options;
+}
 
 async function getPage(isDev: boolean) {
-    if (_page) {
-        return _page;
-    }
-
-    let puppeteer
-    
-    puppeteer = await import('puppeteer-core')
-    
     const options = await getOptions(isDev);
-    const browser = await puppeteer.launch(options);
-    _page = await browser.newPage() as Page;
+    const browser = await playwright.launchChromium(options)
+    const _page = await browser.newPage({
+        viewport : { width: 1200, height: 630 }
+    });
     return _page;
 }
 
 export async function getScreenshot(html: string, isDev: boolean) {
     const page = await getPage(isDev);
-    await page.setViewport({ width: 1200, height: 630 });
-    await page.setContent(html, {waitUntil : 'networkidle0'});
+    await page.setContent(html, {waitUntil : 'networkidle'});
     const file = await page.screenshot({ type : 'png' });
     return file;
 }
